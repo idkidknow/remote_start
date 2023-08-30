6f11daddb8ffb70fa0455a5982f0260ef473aa28
idkana <ifidkidknow@gmail.com>
2023-08-30 13:59:42 +0800

    clearer response

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,22 +1,29 @@
 use std::{net::SocketAddr, sync::Arc};
 
-use axum::{Router, routing::get, Extension};
+use axum::{routing::get, Extension, Router};
 use common::error::Error;
-use tokio::sync::Notify;
+use tokio::sync::{watch, Notify};
+use serde::Serialize;
 
 #[derive(Clone)]
 struct AppState {
     notify_for_start: Arc<Notify>,
+    started_receiver: watch::Receiver<bool>,
 }
 
-pub async fn start(addr: SocketAddr, notify_for_start: Arc<Notify>) -> Result<(), Error> {
+pub async fn start(
+    addr: SocketAddr,
+    notify_for_start: Arc<Notify>,
+    started_receiver: watch::Receiver<bool>,
+) -> Result<(), Error> {
     let cors = tower_http::cors::CorsLayer::new()
         .allow_methods([axum::http::Method::GET, axum::http::Method::POST])
         .allow_origin(tower_http::cors::Any)
         .allow_headers(tower_http::cors::Any);
 
     let state = AppState {
-        notify_for_start
+        notify_for_start,
+        started_receiver,
     };
 
     let app = Router::new()
@@ -37,7 +44,18 @@ async fn root_handler() -> &'static str {
     "/start"
 }
 
+#[derive(Debug, Serialize)]
+struct StartResponse {
+    success: bool,
+    msg: &'static str,
+}
+
 async fn start_handler(Extension(state): Extension<AppState>) -> String {
-    state.notify_for_start.notify_one();
-    "Ok".to_owned()
+    state.notify_for_start.notify_waiters();
+    let started = { *state.started_receiver.borrow() };
+    let response = StartResponse {
+        success: !started,
+        msg: if started { "already started" } else { "success" },
+    };
+    serde_json::to_string(&response).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use api;
 use colored::Colorize;
 use common::error::Error;
 use dotenvy;
-use tokio::{sync::Notify, process::Command};
+use tokio::{sync::{Notify, watch}, process::Command};
 use std::{env, net::SocketAddr, str::FromStr, sync::Arc, process::ExitStatus};
 
 #[tokio::main]
@@ -10,14 +10,15 @@ async fn main() -> Result<(), Error> {
     let _ = dotenvy::dotenv(); // Read .env
     let socket_addr = read_env("ADDR", Some(SocketAddr::from_str("0.0.0.0:25566").unwrap()))?;
     let command: String = read_env("COMMAND", None)?;
+    let (tx, rx) = watch::channel(false); // watch if the subprocess is started
     let notify = Arc::new(Notify::new());
     let notify2 = notify.clone();
     let api_task = tokio::spawn(async move {
         println!("Listening {}", socket_addr.to_string().cyan());
-        api::start(socket_addr, notify).await
+        api::start(socket_addr, notify, rx).await
     });
     let command_task = tokio::spawn(async move {
-        subprocess_loop(&command, notify2).await
+        subprocess_loop(&command, notify2, tx).await
     });
     let results = tokio::join!(api_task, command_task);
     if results.0.is_err() || results.1.is_err() {
@@ -53,9 +54,11 @@ where
     }
 }
 
-async fn subprocess_loop(command: &str, notify: Arc<Notify>) {
+async fn subprocess_loop(command: &str, notify: Arc<Notify>, started_sender: watch::Sender<bool>) {
     loop {
+        started_sender.send(true).expect("API server down");
         let res = run_command(command).await;
+        started_sender.send(false).expect("API server down");
         match res {
             Err(e) => {
                 eprintln!("Error when executing command: {}", e);
